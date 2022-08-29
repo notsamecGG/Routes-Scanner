09dfbc62454aa3675b0a56238bada25737b55372
Sam lin-d <samuel.grulich@gmail.com>
2022-08-29 19:57:40 +0200

    Basic version
    added routes scanning
    added router generator
    
    // there are lots of relative paths included

---
+++ out.ts
@@ -0,0 +1,4 @@
+import manifest from "./routes.gen.ts";
+import router from "./router.gen.tsx";
+
+export default {router, manifest};
---
+++ routerGen.ts
@@ -0,0 +1,62 @@
+import manifest from "./routes.gen.ts";
+import { html } from "./basicHTML.ts";
+import { render } from "preact-render-to-string";
+
+export function createPage(components: Element, title="Document"): string
+{
+    const pageHtml = html;
+    pageHtml.title = title;
+    pageHtml.getElementById("app").innerHTML = render(components);
+
+    return pageHtml.toString();
+}
+
+
+function generateRouter(outFilePath="./router.gen.tsx")
+{
+    let str = `// AUTOMATICALLY GENERATED CODE, regenerates everytime any RouterGenerator is run 
+import { Router, Context } from "oak";
+import { createPage } from "./routerGen.ts";\n`;
+
+    for (const [index, route] of manifest.routes.entries())
+    {
+        const defaultFunc = Object(route.module)["default"];
+        if (!defaultFunc)
+            continue;
+
+        str += `import { default as $${index}Default} from "${route.relativePath}";\n`;
+    }
+
+    str += `\nconst router = new Router(); \n\nrouter`;
+
+    for (const [index, route] of manifest.routes.entries())
+    {
+        const moduleKeys = Object.keys(route.module);
+        let routeConfig = {
+            method: "get",
+            title: "Hello :)",
+        };
+        
+        // unpack config
+        if(moduleKeys.includes("config"))
+        {
+            const { config } = route.module;
+            routeConfig = {...routeConfig, ...config};
+        }
+
+        const defaultFunc = Object(route.module)["default"];
+        const bodyContent = defaultFunc ? `createPage(<$${index}Default/>)` : `"<h1> Empty page <h1/>"`;
+
+        // generate router file :)
+        str += `.${routeConfig.method}("${route.urlPath}", (context: Context) => {
+    context.response.type = "text/html; charset=utf-8";
+    context.response.body = ${bodyContent};
+})`;
+    }
+
+    str += `\n\nexport default router;\n`;
+    Deno.writeTextFile(outFilePath, str);
+    console.log(str);
+}
+
+generateRouter();
